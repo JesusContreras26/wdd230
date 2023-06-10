cdbb4ddfb4d24f9fa1469e430a3da1eb44c5c89c
JesusContreras26 <jgcc26@gmail.com>
2023-06-10 14:07:06 -0400

    change js

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,4 +1,4 @@
-todaysDate = new Date(document.lastModified)
+todaysDate = new Date(document.lastModified);
 dayName = todaysDate.getDay();
 if(dayName === 0){
     dayName += 1;
@@ -7,6 +7,5 @@ if(dayName === 0){
 monthName = todaysDate.getMonth() + 1;
 year = todaysDate.getFullYear();
 time = todaysDate.toLocaleTimeString('it-IT');
-console.log(time);
 document.querySelector('#year').textContent = year;
 document.querySelector('#lastModified').textContent = `Last Modification: ${dayName}/${monthName}/${year} ${time}`;
