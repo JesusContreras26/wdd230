b129020a12cd094bb7223559e9add808ed1bfc2e
JesusContreras26 <jgcc26@gmail.com>
2023-06-13 21:19:46 -0400

    darkmode added

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -14,3 +14,36 @@ hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
+
+const darkMode = document.querySelector('#mode');
+const main = document.querySelector('main');
+const cards = document.querySelectorAll('.card');
+const links = document.querySelectorAll('.link');
+
+darkMode.addEventListener('click', ()=>{
+    if(darkMode.textContent.includes('☑️')){
+        main.style.background = '#000';
+        main.style.color = '#fff';
+        cards.forEach(element => {
+            element.style.background = '#754D4D';
+            element.firstElementChild.style.background = '#072E74';
+            element.firstElementChild.style.color = '#fff';
+        });
+        links.forEach(element =>{
+            element.style.color = '#fff';            
+        })
+        darkMode.textContent = '❎';
+    }else{
+        main.style.background = '#fff';
+        main.style.color = '#000';
+        cards.forEach(element => {
+            element.style.background = '#fff';
+            element.firstElementChild.style.background = '#754D4D';
+            element.firstElementChild.style.color = '#fff';
+        });
+        links.forEach(element =>{
+            element.style.color = '#0000EE';            
+        })
+        darkMode.textContent = '☑️';
+    }
+})
