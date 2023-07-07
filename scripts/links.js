15463e641d14c9495879e7d3f85183eb58219a66
JesusContreras26 <jgcc26@gmail.com>
2023-07-06 22:07:10 -0400

    changes

--- scripts/links.js
+++ scripts/links.js
@@ -1,12 +1,13 @@
 const baseURL = 'https://jesuscontreras26.github.io/wdd230/';
 const linksURL = 'https://jesuscontreras26.github.io/wdd230/data/links.json';
+const ulList = document.querySelector('#links');
 
 async function fetchLinks(){
     try {
         const response = await fetch(linksURL);
         if (response.ok) {
             const data = await response.json();
-            console.log(data);
+            displayLinks(data);
         } else {
             throw Error(await response.text());
         }
@@ -14,3 +15,24 @@ async function fetchLinks(){
         console.log(error);
     }
 }
+
+function displayLinks(data){
+    let weeks = data.weeks
+    weeks.forEach((week) => {
+        let liItem = document.createElement('li');
+        liItem.innerHTML = `${week.week}: `;
+        let counter = 0;
+        week.links.forEach((link)=>{
+            if(counter != week.links.length - 1){
+                liItem.innerHTML += `<a class='link' href="${link.url}">${link.title}</a> | `;
+            }else{
+                liItem.innerHTML += `<a class='link' href="${link.url}">${link.title}</a>`
+            }
+            counter++;
+
+        })
+        ulList.appendChild(liItem);
+    });
+}
+
+fetchLinks();
