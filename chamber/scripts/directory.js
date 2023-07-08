e9c2ecdf61ea601eb2aa8d94cefa3dfb8a0d3a19
JesusContreras26 <jgcc26@gmail.com>
2023-07-08 11:20:02 -0400

    testing

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -37,8 +37,58 @@ async function fetchLinks(){
 
 function displayCards(data){
     data.directory.forEach(member => {
-        
+        let section = document.createElement('section');
+        let nameMember = document.createElement('h4');
+        let addressMember = document.createElement('p');
+        let phoneMember = document.createElement('p');
+        let urlMember = document.createElement('a');
+        let imgMember = document.createElement('img');
+        let membershipMember = document.createElement('p');
+        let joinMember = document.createElement('p');
+
+        nameMember.innerHTML = `${member.name}`
+        imgMember.setAttribute('src', member.image);
+        imgMember.setAttribute('alt', member.name);
+        imgMember.setAttribute('loading', 'lazy');
+        imgMember.setAttribute('width', '160');
+        addressMember.innerHTML = `${member.address}`;
+        phoneMember.innerHTML = `${member.phone}`;
+        urlMember.innerHTML = `${member.url}`;
+        urlMember.setAttribute('href', member.url);
+        membershipMember.innerHTML = `Membership: ${member.membership}`;
+        joinMember.innerHTML = `Member Since: ${member.join}`;
+
+        section.appendChild(imgMember);
+        section.appendChild(nameMember);
+        section.appendChild(addressMember);
+        section.appendChild(phoneMember);
+        section.appendChild(urlMember);
+        section.appendChild(membershipMember);
+        section.appendChild(joinMember);
+
+        ulList.appendChild(section);
     });
 }
 
 fetchLinks();
+
+
+//buttons functionality
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
+
+// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
+
+gridbutton.addEventListener("click", () => {
+	// example using arrow function
+	display.classList.add("grid");
+	display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList); // example using defined function
+
+function showList() {
+	display.classList.add("list");
+	display.classList.remove("grid");
+}
